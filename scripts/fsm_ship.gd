4284cda2dc2fac7bc901572549b15d4a04bd7750
Balthendir <stefanhaak@gmx.net>
2025-01-19 16:48:11 +0100

    Syntax change _on_body_exit

--- scripts/fsm_ship.gd
+++ scripts/fsm_ship.gd
@@ -24,4 +24,4 @@ func _on_deck_body_entered(_body: Node2D) -> void:
 
 func _on_deck_body_exited(_body: Node2D) -> void:
 	if _body is Player and _body.get_parent() != get_node("/root/Level/"):
-		_body.call_deferred("reparent", get_node("/root/Level/"))
+		_body.reparent.call_deferred(get_node("/root/Level/"))
